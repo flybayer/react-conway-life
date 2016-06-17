ffe11a5952e74318725c53e93d4f69936181e3dc
Brandon Bayer <b@bayer.ws>
2016-06-17 13:39:59 -0500

    test: add basic testing with tape

--- tests/cell.js
+++ tests/cell.js
@@ -1,26 +1,13 @@
 //https://gist.github.com/ericelliott/bc4c0fb66973202f6680#file-answer-unit-test-questions-js
 import test from 'tape';
-import cell from '../js/cell';
+import cell from '../src/cell';
 
-test('new cell is dead', assert => {
+test('new cell', assert => {
+  const msg = 'should be dead'
+
   const actual = cell().isAlive();
   const expected = false;
 
-  assert.equal(actual, expected,
-    'a new cell should not be alive');
-
+  assert.same(actual, expected, msg);
   assert.end();
 });
-
-
-
-
-// test('What component aspect are you testing?', assert => {
-//   const actual = 'What is the actual output?';
-//   const expected = 'What is the expected output?';
-//
-//   assert.equal(actual, expected,
-//     'What should the feature do?');
-//
-//   assert.end();
-// });
